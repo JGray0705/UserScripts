f41af230392e906a629c53e7fd4b6f8802a82ba9
Jeffery Gray <35303555+JGray0705@users.noreply.github.com>
2020-09-22 21:30:35 -0700

    Add files via upload

---
+++ PalletCounter.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Pallet Counter
+// @namespace    https://aftlite-na.amazon.com
+// @version      1.0
+// @description  Add pallet count to receive inventory page
+// @author       grajef@
+// @match        https://aftlite-na.amazon.com/receive_inventory/show*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var pallets = document.body.getElementsByTagName("tbody")[2]; // pallets are listed in the third tbody on the page
+    var count = pallets.children[1].children[2].children.length - 2;
+    pallets.children[1].children[0].innerText += " (" + count + ")";
+})();
