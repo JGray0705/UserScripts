6a1ace4ba895abbd61d471ead82028cd86c09ca8
Jeffery Gray <Jgray0705@gmail.com>
2020-10-08 15:58:49 -0700

    Add files via upload

--- InventoryAddOn.user.js
+++ InventoryAddOn.user.js
@@ -8,23 +8,25 @@
 // @match        https://aftlite-na.amazon.com/inventory/view_inventory_at*
 // @match        https://aftlite-na.amazon.com/inventory/view_catalog_data_for_asin*
 // @match        https://aftlite-portal.amazon.com/inventory/view_inventory_for_asin_display*
+// @match        https://aftlite-portal.amazon.com/inventory/view_inventory_at*
+// @match        https://aftlite-portal.amazon.com/inventory/view_catalog_data_for_asin*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     // just copy of the HTML from the inventory page
-    var tables = document.getElementsByTagName("table");
-    var table = tables[tables.length - 1];
-    var row1 = document.createElement("tr");
-    var row2 = document.createElement("tr");
-    var row3 = document.createElement("tr");
-    row1.innerHTML = '<td><form accept-charset="UTF-8" action="/inventory/view_inventory_for_asin" method="post">Inventory by ASIN or UPC <input type="text" name="asin" size="20"><input type="submit" name="view" value="view or update"/></form></td>'
-    row2.innerHTML = '<td><form accept-charset="UTF-8" action="/inventory/view_inventory_at" method="post">Inventory by Location <input type="text" name="location_name" size="20"/><input type="submit" name="view" value="view or move"/></form></td>'
-    row3.innerHTML = '<td><form accept-charset="UTF-8" action="/inventory/view_catalog_data_for_asin" method="post">Catalog or weight data by ASIN <input type="text" name="asin" size="20"/><input type="submit" name="view" value="View"/></form></td>';
-    tables[1].appendChild(row1);
-    tables[1].appendChild(row2);
-    tables[1].appendChild(row3);
-    if(window.location.href.indexOf("aftlite-portal" <= -1)) { // check for portal page to add some classes for css
+    if(window.location.href.match("aftlite-portal")) { // check for portal page to add some classes for css
+        var mainDiv = document.getElementById("main-content");
+        if(!mainDiv) { mainDiv = document.getElementById("a-page"); }
+        var portalRow1 = document.createElement("div");
+        var portalRow2 = document.createElement("div");
+        var portalRow3 = document.createElement("div");
+        portalRow1.innerHTML = '<form accept-charset="UTF-8" action="/inventory/view_inventory_for_asin" method="post">Inventory by ASIN or UPC <input type="text" name="asin" size="20"><input type="submit" name="view" value="view or update"/></form>'
+        portalRow2.innerHTML = '<form accept-charset="UTF-8" action="/inventory/view_inventory_at" method="get">Inventory by Location <input type="text" name="location_name" size="20"/><input type="submit" name="view" value="view or move"/></form>'
+        portalRow3.innerHTML = '<form accept-charset="UTF-8" action="/inventory/view_catalog_data_for_asin" method="post">Catalog or weight data by ASIN <input type="text" name="asin" size="20"/><input type="submit" name="view" value="View"/></form>';
+        mainDiv.appendChild(portalRow1);
+        mainDiv.appendChild(portalRow2);
+        mainDiv.appendChild(portalRow3);
         var buttons = document.querySelectorAll('input[type="submit"]');
         buttons.forEach(x => {
             x.classList.add("a-button-primary");
@@ -32,14 +34,27 @@
             x.style.borderRadius = '2px';
             x.style.padding = '1px';
         }); // make all of the buttons match
-        if(window.location.href.indexOf("view_inventory_for_asin_display") <= -1) { // asin page for AFTLite-Portal
+        if(window.location.href.match("view_inventory_for_asin_display")) { // asin page for AFTLite-Portal
             var portalAsinHeader = document.getElementsByTagName("h3")[0].innerHTML.split(" ");
             addImage(portalAsinHeader);
         }
     }
-    else if(window.location.href.indexOf("view_inventory_for_asin") <= -1) { // add image to asin page for AFTLite-na
-        var asinHeader = document.getElementsByTagName("h2")[0].innerHTML.split(" ");
-        addImage(asinHeader);
+    else {
+        var tables = document.getElementsByTagName("table");
+        var table = tables[tables.length - 1];
+        var row1 = document.createElement("tr");
+        var row2 = document.createElement("tr");
+        var row3 = document.createElement("tr");
+        row1.innerHTML = '<td><form accept-charset="UTF-8" action="/inventory/view_inventory_for_asin" method="post">Inventory by ASIN or UPC <input type="text" name="asin" size="20"><input type="submit" name="view" value="view or update"/></form></td>'
+        row2.innerHTML = '<td><form accept-charset="UTF-8" action="/inventory/view_inventory_at" method="post">Inventory by Location <input type="text" name="location_name" size="20"/><input type="submit" name="view" value="view or move"/></form></td>'
+        row3.innerHTML = '<td><form accept-charset="UTF-8" action="/inventory/view_catalog_data_for_asin" method="post">Catalog or weight data by ASIN <input type="text" name="asin" size="20"/><input type="submit" name="view" value="View"/></form></td>';
+        tables[1].appendChild(row1);
+        tables[1].appendChild(row2);
+        tables[1].appendChild(row3);
+        if(window.location.href.match("view_inventory_for_asin")) { // add image to asin page for AFTLite-na
+            var asinHeader = document.getElementsByTagName("h2")[0].innerHTML.split(" ");
+            addImage(asinHeader);
+        }
     }
 })();
 
