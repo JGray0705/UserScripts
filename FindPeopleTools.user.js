6812ec70b9599dd40bb3d3eb91c95f40f2fb899c
Jeffery Gray <Jgray0705@gmail.com>
2020-10-09 00:24:24 -0700

    Add files via upload

--- FindPeopleTools.user.js
+++ FindPeopleTools.user.js
@@ -5,12 +5,45 @@
 // @description  Add auto refresh and highlight idle logins
 // @author       grajef@
 // @match        https://aftlite-na.amazon.com/labor_tracking/find_people*
+// @match        https://aftlite-portal.amazon.com/labor_tracking/find_people*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    var table = document.getElementById("recent_event_table");
-    for(let row of table.rows) {
+    if(window.location.href.match("aftlite-na")) {
+        var table = document.getElementById("recent_event_table");
+        searchTable(table);
+        var cb = getCheckbox();
+        var label = document.createElement("label");
+        label.innerHTML = "Auto Refresh";
+        var p = document.getElementById("content");
+        p.before(label);
+        p.before(cb);
+    }
+    else {
+        var portalTable = document.getElementsByTagName("table")[0];
+        searchTable(portalTable);
+        var cbLabel = document.createElement("label");
+        var check = getCheckbox();
+        check.style.float = "left";
+        check.style.marginLeft = "10px";
+        check.style.marginRight = "4px";
+        cbLabel.innerHTML = "Auto Refresh";
+        var main = document.getElementById("main-content");
+        main.before(check);
+        check.after(cbLabel);
+    }
+})();
+function getCheckbox() {
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.onClick = autoReload(checkbox);
+    checkbox.checked = true;
+    return checkbox;
+}
+
+function searchTable(t) {
+    for(let row of t.rows) {
         var cell = row.cells[7].innerHTML;
         var time = Number(row.cells[5].innerHTML.split("mins")[0]);
         if(time >= 10 || (time >= 15 && cell.includes("BRK"))) {
@@ -19,18 +52,7 @@
             }
         }
     }
-
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    var p = document.getElementById("content");
-    var label = document.createElement("label");
-    label.innerHTML = "Auto Refresh";
-    p.before(label);
-    p.before(checkbox);
-    checkbox.onClick = autoReload(checkbox);
-    checkbox.checked = true;
-})();
-
+}
 function autoReload(checkbox) {
     setInterval(function() {
         if(checkbox.checked) {
