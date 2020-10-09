12e7af8e717be2ff692bab6062f143817a53fc6e
Jeffery Gray <Jgray0705@gmail.com>
2020-10-08 23:25:29 -0700

    Add files via upload

--- NYRDashboardColors.user.js
+++ NYRDashboardColors.user.js
@@ -2,13 +2,14 @@
 // @name         NYRDashboardColors
 // @namespace    https://github.com/jgray0705/userscripts
 // @version      1.0
-// @description  try to take over the world!
+// @description  Add SLA colors and auto refresh to the NYR dashboard
 // @author       grajef@
 // @match        https://aftlite-na.amazon.com/inbound/nyr_dashboard
 // @grant        none
 // ==/UserScript==
 
 (function() {
+    // set the colors
     var table = document.getElementById("details");
     var col = table.getElementsByTagName("th")[7];
     col.click();
@@ -26,4 +27,22 @@
             row.cells[7].style.background = '#56d12a';
         }
     };
+    // do auto reload things
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    var p = document.getElementsByTagName("p")[0];
+    var label = document.createElement("label");
+    label.innerHTML = "Auto Refresh";
+    p.after(label);
+    p.after(checkbox);
+    checkbox.onClick = autoReload(checkbox);
+    checkbox.checked = true;
 })();
+
+function autoReload(checkbox) {
+    setInterval(function() {
+        if(checkbox.checked) {
+            location.reload();
+        }
+    }, 30000);
+}
