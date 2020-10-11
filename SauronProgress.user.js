d8eec0cae7c7d5ae677d52eb5555247c5adb41a2
Jeffery Gray <Jgray0705@gmail.com>
2020-10-10 21:41:41 -0700

    Add files via upload

--- SauronProgress.user.js
+++ SauronProgress.user.js
@@ -12,20 +12,20 @@
 (function() {
     let s = document.getElementsByTagName("span")[0];
     let green = document.createElement("p");
-    let yellow = document.createElement("p");
+    let blue = document.createElement("p");
     let red = document.createElement("p");
 
     green.innerHTML = "A green progress bar indicates batching is on time.";
-    yellow.innerHTML = "A yellow progress bar indicates batching is ahead.";
+    blue.innerHTML = "A blue progress bar indicates batching is ahead.";
     red.innerHTML = "A red progress bar indicates batching is behind.";
 
     green.style.color = "green";
-    yellow.style.color = "#ccc937";
+    blue.style.color = "#5d9def";
     red.style.color = "red";
 
     s.after(green);
-    green.after(yellow);
-    yellow.after(red);
+    green.after(blue);
+    blue.after(red);
 
     setInterval(function() {
         let progress = document.getElementsByTagName("b")[1].innerHTML.split("(")[1].split(")")[0].split("%")[0];
@@ -40,7 +40,8 @@
         else {
             let t = new Date();
             let min = t.getMinutes() - 4; // since batching starts ~ 4 minutes after the hour,
-            min = min < 0 ? 0 : min;
+            min = min < 0 ? 0 : min * 100; // min is a decimal but progress is a whole number (x% vs. .x)
+
             let timePercent = min / 30;
             var bars = document.querySelectorAll(".progress-bar");
             if(timePercent > progress) {
@@ -54,7 +55,7 @@
                 // ahead, too many batchers. set to yellow
                 for(let bar of bars) {
                     bar.classList.remove("bg-success");
-                    bar.style.backgroundColor = "yellow";
+                    bar.style.backgroundColor = "#5d9def";
                 }
             }
             else {
@@ -65,5 +66,5 @@
                 }
             }
         }
-    }, 10000);
+    }, 5000);
 })();
