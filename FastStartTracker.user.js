090a828347767d96d6b37ecae038c2300d64c3fe
Jeffery Gray <Jgray0705@gmail.com>
2021-02-18 15:56:07 -0700

    Change ADMN to START

--- FastStartTracker.user.js
+++ FastStartTracker.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         FastStartTracker
 // @namespace    https://github.com/jgray0705/UserScripts
-// @version      1.0
-// @description  Highlight people in ADMN for more than 5 minutes to help track fast start
+// @version      2.0
+// @description  Highlight people in START for more than 5 minutes to help track fast start
 // @author       grajef@
 // @match        https://aftlite-na.amazon.com/labor_tracking/find_people*
+// @match        https://aftlite-portal.amazon.com/labor_tracking/find_people*
 // @grant        none
 // ==/UserScript==
 
@@ -14,7 +15,7 @@
         searchTable(table);
     }
     else {
-        let portalTable = document.getElementsByTagName("table")[0];
+        let portalTable = document.getElementsByTagName("tbody")[0];
         searchTable(portalTable);
     }
 })();
@@ -24,7 +25,11 @@ function searchTable(t) {
         if(row.rowIndex <= 1) continue; // first 2 rows of the table are not important
         let cell = row.cells[7].innerHTML.trim();
         let time = Number(row.cells[5].innerHTML.split("mins")[0]);
-        if(cell.includes("ADMN") && time >= 5) {
+        if(cell.includes("START") && time >= 5) {
+            if(window.location.href.match("aftlite-portal")) {
+                row.cells[5].style.color = "white";
+                row.cells[5].classList.remove("a-color-price");
+            }
             row.cells[5].style.background = "purple";
         }
     }
